333b33d4b4a1b8a11771dc1f610ac18392cbe57a
Beatriz Teixeira <beatrizteisi@gmail.com>
2022-06-29 17:55:44 -0300

    primeiro commit

---
+++ src/query/desempenhoConstructor-4.sql
@@ -0,0 +1,20 @@
+SELECT
+	GP.`year`,
+	equipes.`name`,
+	podioEquipes.position
+FROM
+    constructorstandings AS podioEquipes
+    INNER JOIN races AS GP ON GP.raceId = podioEquipes.raceId
+    INNER JOIN constructors AS equipes ON equipes.constructorId = podioEquipes.constructorId
+WHERE
+	podioEquipes.raceId IN (
+		SELECT
+			raceId
+		FROM
+			(SELECT `year`, MAX(round) AS round FROM races GROUP BY `year` HAVING `year` >= 2000) AS finais
+			INNER JOIN races AS GP ON (GP.`year` = finais.`year` AND GP.round = finais.round)
+	)
+ORDER BY 
+	GP.`year` ASC,
+    podioEquipes.raceId ASC,
+    podioEquipes.position ASC;
